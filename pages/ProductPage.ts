55c66f92c326d56d57551b904664a4d38dec615a
Cassandra Vellema <cassandra.vellema@soliditech.com>
2026-07-08 20:12:47 +0200

    feat: complete category and brand audit tests with strict POM separation

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -8,6 +8,8 @@ export class ProductPage extends BasePage {
     private productSearchButton: Locator;
     private productSearchHeader: Locator;
     private productCards: Locator;
+    private productCategoryAccordion: Locator;
+    private brandsAccordion: Locator;
 
     constructor(page: Page) {
         super(page);
@@ -16,13 +18,19 @@ export class ProductPage extends BasePage {
         this.productSearchButton = page.locator("#submit_search");
         this.productSearchHeader = page.getByRole("heading", {name:"Searched Products"});
         this.productCards = page.locator(".single-products");
+        this.productCategoryAccordion = page.locator(".category-products");
+        this.brandsAccordion = page.locator(".brands-name");
 
     }
 
     async searchProduct(product: string) {
         await this.productSearchInput.fill(product);
         await this.productSearchButton.click();
+    }
 
+    async selectCategoryAndSubcategory(cat: string, subcat: string) {
+        await this.productCategoryAccordion.getByRole("link", { name: cat, exact: false }).click();
+        await this.productCategoryAccordion.locator("#"+cat).getByRole("link", { name: subcat, exact: false }).click();
     }
 
     async getSearchHeaderText() {
@@ -34,12 +42,22 @@ export class ProductPage extends BasePage {
     }
 
     async getFirstSearchResult() {
-    return this.productCards.first();
-}
+        return this.productCards.first();
+    }
 
-    async getHoverElementAndAddToCart(): Promise<void> {
-    const targetCard = await this.getFirstSearchResult();
-    await targetCard.hover();
-    await targetCard.locator(".product-overlay").getByText("Add to cart").click();
-}
+    async getHoverElementAndAddToCart() {
+        const targetCard = await this.getFirstSearchResult();
+        await targetCard.hover();
+        await targetCard.locator(".product-overlay").getByText("Add to cart").click();
+    }
+
+    async getExpectedBrandCount(brandName: string) {
+        const brandLocator = this.brandsAccordion.getByRole("link", { name: brandName });
+        const fullString = await brandLocator.innerText();
+        return parseInt(fullString.replace(/\D/g, ""), 10);
+    }
+
+    async clickBrand(brandName: string) {
+        await this.brandsAccordion.getByRole("link", { name: brandName }).click();
+    }
 }
--- tests/ui/ui-products.spec.ts
+++ tests/ui/ui-products.spec.ts
@@ -5,7 +5,7 @@ test.beforeEach(async ({page}) => {
     await page.goto("https://automationexercise.com/products");
 });
 
-test('Test 1: Validated Product Search', async ({ page }) => {
+test("Test 1: Validated Product Search", async ({ page }) => {
     const productPage = new ProductPage(page);
 
     await productPage.searchProduct("Blue Top");
@@ -20,7 +20,7 @@ test('Test 1: Validated Product Search', async ({ page }) => {
     await expect(firstSearchResult).toContainText("Blue Top");
 });
 
-test('Test 3: Cart Addition via Hover Overlay', async ({ page }) => {
+test("Test 2: Cart Addition via Hover Overlay", async ({ page }) => {
     const productPage = new ProductPage(page);
 
     await productPage.searchProduct("Blue Top");
@@ -32,5 +32,35 @@ test('Test 3: Cart Addition via Hover Overlay', async ({ page }) => {
 
     await expect(page.locator(".modal-confirm")).toBeVisible();
     await expect(page.locator(".modal-confirm")).toContainText("Your product has been added to cart.")
+});
+
+test("Test 3: Category & Subcategory Drill-Down", async ({ page }) => {
+    const productPage = new ProductPage(page);
+    const categoryString = "Women";
+    const subcategoryString = "Dress";
+
+    await productPage.selectCategoryAndSubcategory(categoryString, subcategoryString);
+
+    await expect(page).toHaveURL(/.*\/category_products\/1.*/);
+
+    const searchResultsCount = await productPage.getProductResultsCount();
+    expect(searchResultsCount).toBeGreaterThan(0);
+    const firstSearchResult = await productPage.getFirstSearchResult();
+    await expect(firstSearchResult).toContainText("Sleeveless Dress");
+});
+
+test("Test 4: Brand Product Count Audit", async ({ page }) => {
+    const productPage = new ProductPage(page);
+    const brandName = "Polo"
+    let extractedNumber = 0;
+
+    extractedNumber = await productPage.getExpectedBrandCount(brandName);
+    await productPage.clickBrand(brandName);
+    const searchResultsCount = await productPage.getProductResultsCount();
+    expect(searchResultsCount).toEqual(extractedNumber);
+
+    // The Golden Rule of POM: 
+    // Test scripts (.spec.ts) should never contain 
+    // raw CSS selectors, XPaths, or UI locators.
     
 });
