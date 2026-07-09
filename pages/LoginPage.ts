32e6f20c774b9365cf20332a6e3c10484de1f127
Cassandra Vellema <cassandra.vellema@soliditech.com>
2026-07-09 19:27:21 +0200

    feat: add login tests: happy path, bad password, signup redirect

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -5,19 +5,52 @@ export class LoginPage extends BasePage {
     private loginEmailInput: Locator;
     private loginPasswordInput: Locator; 
     private loginButton: Locator;
+    private loginForm: Locator;
+
+    private signupForm: Locator;
+    private signupNameInput: Locator;
+    private signupEmailInput: Locator;
+    private signupButton: Locator;
+
+    private navbar: Locator
 
     constructor(page: Page) {
         super(page);
 
-        this.loginEmailInput = page.locator("form").filter({hasText: "Login"}).getByPlaceholder("Email Address");
+        this.loginForm = page.locator(".login-form");
+        this.loginEmailInput = this.loginForm.getByPlaceholder("Email Address");
         this.loginPasswordInput = page.getByPlaceholder("Password");
         this.loginButton = page.getByRole("button", {name: "Login"});
+        this.navbar = page.locator(".shop-menu");
+       
+        this.signupForm = page.locator(".signup-form");
+        this.signupNameInput = page.getByPlaceholder("Name");
+        this.signupEmailInput = this.signupForm.getByPlaceholder("Email Address");
+        this.signupButton = page.getByRole("button", {name: "Signup"});
     }
 
     async login(email: string, password: string) {
         await this.loginEmailInput.fill(email);
         await this.loginPasswordInput.fill(password);
         await this.loginButton.click();
+    }
+
+    async signup(name: string, email: string) {
+        await this.signupNameInput.fill(name);
+        await this.signupEmailInput.fill(email);
+        await this.signupButton.click();
+    }
+
+    async findLoginConfirmation(name: string) {
+        return await this.navbar.locator("li").last().innerText();
+    }
+
+    async getLoginForm() {
+        return this.loginForm;
+    }
+
+    getErrorMessageLocator(): Locator {
+        return this.loginForm.getByText("Your email or password is incorrect!");
     }
 
 }
---
+++ tests/ui/ui-login.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../pages/LoginPage';
+
+test.beforeEach(async ({page}) => {
+    await page.goto("https://automationexercise.com/login");
+});
+
+test("Test 1: Valid Login Happy Path", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    const password = "playwright123";
+    const email  = "playwrightpractise@cv.com";
+    const name = "Playwright CV";
+
+    await loginPage.login(email, password);
+    expect(page).toHaveURL("https://automationexercise.com");
+    const loginConfirmationElement = await loginPage.findLoginConfirmation(name);
+    expect(loginConfirmationElement).toContain("Logged in as "+name);
+
+
+    
+});
+
+test("Test 2: Invalid Login: Bad Password", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    const password = "error123987!";
+    const email  = "playwrightpractise@cv.com";
+
+    await loginPage.login(email, password);
+    const errorBanner = loginPage.getErrorMessageLocator();
+    await expect(errorBanner).toBeVisible();
+    
+});
+
+test("Test 3: Sign Up New User (Not Full Journey)", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    const name = "Portfolio QA";
+    const email  = "playwrightpractise2@cv.com";
+
+    await loginPage.signup(name, email);
+    await expect(page).toHaveURL("https://automationexercise.com/signup");
+    
+});
